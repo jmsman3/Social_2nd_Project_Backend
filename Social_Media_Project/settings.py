349ede36af75442bfbad50500da94352fa2466c5
Md. Jubaer Mahmud Sarker <jmsman3@gmail.com>
2024-10-08 19:20:20 +0600

    static done

--- Social_Media_Project/settings.py
+++ Social_Media_Project/settings.py
@@ -171,7 +171,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 # settings.py
